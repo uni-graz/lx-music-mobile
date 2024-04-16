131b8bdb829980a312697a7d61e2409837177b48
ikun <ikun0014@qq.com>
2024-04-16 19:12:19 +0800

    re: 写乱了，重来

--- src/utils/music.ts
+++
@@ -1,6 +0,0 @@
-import { existsFile } from './fs'
-
-
-export const getLocalFilePath = async(musicInfo: LX.Music.MusicInfoLocal): Promise<string> => {
-  return (await existsFile(musicInfo.meta.filePath)) ? musicInfo.meta.filePath : ''
-}
