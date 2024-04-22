d9261c6b91711a80aa3a8fe3bdbf9810a820322f
zx <834688422@qq.com>
2024-04-22 20:04:06 +0800

    去除更新检测

--- src/screens/Home/Views/Setting/settings/Version.tsx
+++ src/screens/Home/Views/Setting/settings/Version.tsx
@@ -21,7 +21,7 @@ export default memo(() => {
   const progress = useVersionDownloadProgressUpdated()
   const handleOpenVersionModal = () => {
     // setVersionInfo({ showModal: true })
-    showModal()
+    // showModal()
   }
 
   useEffect(() => {
